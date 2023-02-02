3cd0c9fafab5a19ebea285686b4979d1d2c95691
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-02-02 10:29:49 -0300

    finalizaçao do codigo

--- js/index.js
+++ js/index.js
@@ -24,17 +24,49 @@ class Calculator {
 
     // Process all calculator operation
     processOperation(operation) {
+        // Check if current is empty
+        if (this.currentOperationText.innerText === "" && operation !== "C") {
+            // Change  Operation
+            if (this.previousOperationText.innerText !== "") {
+                this.changeOperation(operation);
+            }
+            return
+        }
 
 
         // Get Current and previous value
         let operationValue
-        const previous = +this.previousOperationText.innerText
+        const previous = +this.previousOperationText.innerText.split(" ")[0]
         const current = +this.currentOperationText.innerText
 
         switch (operation) {
             case "+":
                 operationValue = previous + current
                 this.updateScreen(operationValue, operation, current, previous)
+                break
+            case "-":
+                operationValue = previous - current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            case "*":
+                operationValue = previous * current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            case "/":
+                operationValue = previous / current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            case "DEL":
+                this.processDelOperator();
+                break
+            case "C":
+                this.processCOperator();
+                break
+            case "CE":
+                this.processCEOperator();
+                break
+            case "=":
+                this.processEqualOperator();
                 break
             default:
                 return;
@@ -44,18 +76,61 @@ class Calculator {
 
     // Change values of the caluculator screen
     updateScreen(
-        operationValue = null, 
-        operation = null, 
-        current = null, 
+        operationValue = null,
+        operation = null,
+        current = null,
         previous = null
-        ) {
+    ) {
 
-            console.log(operationValue, operation, current, previous)
-
-            if(operationValue == null){
-                this.currentOperationText.innerText += this.currentOperation;
+        if (operationValue == null) {
+            this.currentOperationText.innerText += this.currentOperation;
+        } else {
+            // Check if value is zero, if it is just add current value
+            if (previous === 0) {
+                operationValue = current
             }
-        
+
+            // Add current value to previous
+            this.previousOperationText.innerText = `${operationValue} ${operation}`
+            this.currentOperationText.innerText = "";
+        }
+
+    }
+
+    // Change math operation
+    changeOperation(operation) {
+        const mathOperations = ["+", "-", "/", "*"]
+
+        if (!mathOperations.includes(operation)) {
+            return;
+        }
+
+        this.previousOperationText.innerText =
+            this.previousOperationText.innerText.slice(0, -1) + operation;
+    }
+
+    // Delete the last digit
+    processDelOperator() {
+        this.currentOperationText.innerText =
+            this.currentOperationText.innerText.slice(0, -1);
+    }
+
+    // Clear current operation
+    processCEOperator() {
+        this.currentOperationText.innerText = "";
+    }
+
+    // Clear current and previous operation
+    processCOperator() {
+        this.currentOperationText.innerText = "";
+        this.previousOperationText.innerText = "";
+    }
+
+    // Process an operation
+    processEqualOperator() {
+        const operation = previousOperationText.innerText.split(" ")[1]
+
+        this.processOperation(operation);
     }
 }
 
